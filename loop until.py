a7abddfacae96aa68b30adec86a41ecf46e5d007
swathi1913 <60419534+swathi1913@users.noreply.github.com>
2020-01-29 14:48:36 +0530

    loop until.py
    it execute the value until q

---
+++ loop until.py
@@ -0,0 +1,107 @@
+s=raw_input("enter the value:")
+while(True):
+    if(s=="q"):
+        break
+    else:
+        print(s)
+
+    '''
+    enter the value:e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+eenter the value:e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+e
+
+enter the value:q
+>>> 
+'''
